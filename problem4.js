fc630121a22b93c3c6c65124b0b13ca6490b9ac9
Jake Elizondo <jake.elizondo23@gmail.com>
2021-02-11 16:50:55 -0500

    GOT THE STRING REVERSAL

---
+++ problem4.js
@@ -0,0 +1,5 @@
+// Analyze the following function (without running it in an IDE) to determine what problem it is trying to solve. What is the time complexity of this algorithm?
+
+// I believe this function removes duplicates from a list.
+
+// I want to say that the function would be O(n^2) because the runtime depends on the length of the list being purged of duplicates, but its running on n characters in the list. So n*n would be O(n^n). Best case run time would be O(1) if there is only one item.
